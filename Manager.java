76d5b9e4ba51659a511c5e88f3642af1cca92583
CeciliaQuan <40803038+CeciliaQuan@users.noreply.github.com>
2018-07-10 12:55:15 -0700

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,22 @@
+
+
+public interface Employee = {
+  var manager = {
+   hours = 40;
+   salary = 100,000;
+   occupation = manager;
+
+
+  }
+
+  public int getHours ( ) {
+    return hours;
+    }
+  public double getSalary ( ) {
+    return salary;
+    }
+  public String occupationName (){
+    return occupation;
+  }
+
+  }

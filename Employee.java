6870d7654aeed3a7032efbfb9818ead197e61349
PranavPusarla <36783092+PranavPusarla@users.noreply.github.com>
2018-07-10 10:46:07 -0700

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public interface Employee{
-  public int getHours(int hours);
-  public double getSalary(double salary);
-  public String occupationName(String name);
+  public int getHours();
+  public double getSalary();
+  public String occupationName();
 }

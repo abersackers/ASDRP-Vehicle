1f59575943154eddb905b0fcb6fcccdfc8961051
PranavPusarla <36783092+PranavPusarla@users.noreply.github.com>
2018-07-10 10:55:30 -0700

    Add files via upload

--- accountant.java
+++ accountant.java
@@ -1,10 +1,20 @@
 public class accountant {
+
   int hours;
   hours = 40
   double salary;
   salary = 65000
   string occupationName;
   occupationName = "accountant"
+  public int getHours() {
+    return hours
+  }
+  public double salary() {
+    return salary
+  }
+  public string occupationName() {
+    return occupationName
+  }
 
 
 }

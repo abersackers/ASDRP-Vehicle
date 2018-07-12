341ddab01bbfe1a5ba29f18001266949cd476175
PranavPusarla <36783092+PranavPusarla@users.noreply.github.com>
2018-07-11 21:36:15 -0700

    Add files via upload

---
+++ test_run.java
@@ -0,0 +1,11 @@
+import accountant.java;
+public class test_run {
+
+  public static void main(String args[]) {
+    accountant myAccountant = new accountant(50, 100000, "super accountant");
+
+
+
+
+  }
+}

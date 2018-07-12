5bfef2c77c0b163de875e9f3c41e5cca00a5b8b9
RyanC1681 <40802485+RyanC1681@users.noreply.github.com>
2018-07-12 11:17:29 -0700

    Add files via upload

---
+++ Salesman.java
@@ -0,0 +1,21 @@
+import.java.util.Scanner;
+public class Salesman implements Employee
+{
+  public int hours = 40;
+  public double salary = 90000.00;
+  public String occupation = salesman;
+
+  public int getHours()
+  {
+    return hours;
+  }
+
+  public double getSalary()
+  {
+    return salary;
+  }
+
+  public String occupationName()
+  {
+    return occupation;
+  }

79786a931379a527bdea5132d3c762254b020251
parshvajain1703 <37520686+parshvajain1703@users.noreply.github.com>
2018-07-14 13:32:40 -0700

    Janitor.java

---
+++ Janitor.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Janitor implements Employee {
+
+	private int hours = 40;
+	private double salary = 20000.0;
+	private String occupation = "Janitor";
+
+
+	public janitor(int hours, double salary, String occupation){
+
+		hours = this.hours;
+		salary = this.salary;
+		occupation = this.occupation;
+
+	}
+
+
+	public getHours(){
+		return hours;
+
+	}
+
+
+	public setHours(int hours){
+		hours = this.hours;
+
+	}
+
+	public getSalary(){
+		return salary;
+
+	}
+
+
+	public setHours(double salary){
+		salary = this.salary;
+
+	}
+
+	public getOccupation(){
+		return occupation;
+
+	}
+
+
+	public occupationName(String occupation){
+		occupation = this.occupation;
+
+	}
+
+
+

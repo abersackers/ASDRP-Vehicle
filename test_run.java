a1b23c28145cd43f1c14885bd163b3be7594722a
CeciliaQuan <40803038+CeciliaQuan@users.noreply.github.com>
2018-07-14 13:25:29 -0700

    Add files via upload

--- test_run.java
+++ test_run.java
@@ -1,8 +1,11 @@
 import accountant.java;
+import Manager.java;
 public class test_run {
 
   public static void main(String args[]) {
     accountant myAccountant = new accountant(50, 100000, "super accountant");
+    manager myManager = new manager(40, 100000, "manager");
+
 
 
 

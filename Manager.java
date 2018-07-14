efdba3619c9c7fa66b69d27a235cc0ba5bf21132
CeciliaQuan <40803038+CeciliaQuan@users.noreply.github.com>
2018-07-14 13:02:43 -0700

    Add files via upload

--- Manager.java
+++ Manager.java
@@ -1,22 +1,45 @@
+public class manager extends Employee {
 
 
-public interface Employee = {
-  var manager = {
-   hours = 40;
-   salary = 100,000;
-   occupation = manager;
 
+  int hours = 40;
+
+  double salary = 100000;
+
+  String occupationName = "manager";
+
+
+
+  public manager(int hrs, double slry, String occName) {
+
+    hours = hrs;
+
+    salary = slry;
+
+    occupationName = occName;
 
   }
 
-  public int getHours ( ) {
+
+
+  public int getHours() {
+
     return hours;
-    }
-  public double getSalary ( ) {
-    return salary;
-    }
-  public String occupationName (){
-    return occupation;
-  }
 
   }
+
+  public double salary() {
+
+    return salary;
+
+  }
+
+  public String occupationName() {
+
+    return occupationName;
+
+  }
+
+
+
+}
